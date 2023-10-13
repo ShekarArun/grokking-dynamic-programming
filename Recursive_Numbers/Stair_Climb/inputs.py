e1f31011a1793bebb4f1c92b47bdc3f1484c69b2
ShekarArun <shekar.arun@gmail.com>
2023-10-13 23:37:25 +0530

    feat: Add Stair Climb naive solution

---
+++ Recursive_Numbers/Stair_Climb/inputs.py
@@ -0,0 +1,10 @@
+# These are the test inputs for the Staircase Climb problem
+
+test1 = {"n": 0}
+test2 = {"n": 3}
+test3 = {"n": 4}
+test4 = {"n": 5}
+test5 = {"n": 6}
+test6 = {"n": 100}
+
+tests = [test1, test2, test3, test4, test5, test6]
